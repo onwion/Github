de137723d3b72190a3c331f994d6ecb42adfcf56
Dmitrii <infosvyaznoy@mail.ru>
2018-10-07 18:15:50 +0300

    UpDate

---
+++ #Current.js
@@ -0,0 +1,8 @@
+var id_contracts = "https://vk.com/search?c[group]=";
+var number1 = "171197882"; 
+var number2 = "&c[per_page]=40&c[photo]=1&c[q]=%D0%90%D0%BB%D0%B8%D0%BD%D0%BE%D1%87%D0%BA%D0%B0%20%D0%92%D0%B8%D1%85%D0%BE%D1%80&c[section]=people"; 
+
+window.open 
+content.document.location.href = ""+id_contracts+""+number1+""+number2+"";
+
+
---
+++ соцпаблик инста.js
@@ -0,0 +1,35 @@
+﻿ //////////////////////////////////////////////////////////////////////////////////////////////////ссылка 1
+ window.open 
+ content.document.location.href = "         https://www.instagram.com/***********/             ";
+ //////////////////////////////////////////////////////////////////////////////////////////////////ссылка 1
+
+
+function stateChange(newState) {
+    setTimeout(function () {
+        if (newState == -1) {
+            alert('VIDEO HAS STOPPED');
+        }
+    }, 7000);
+}
+
+var settings = (`
+	SET !REPLAYSPEED FAST
+	SET !TIMEOUT 360
+	SET !TIMEOUT_STEP 1
+	SET !EXTRACT_TEST_POPUP NO
+	SET !ERRORIGNORE YES
+	`)
+iimPlayCode(`
+	${settings}
+TAG POS=1 TYPE=BUTTON ATTR=TXT:Подписки
+EVENT TYPE=CLICK SELECTOR="HTML>BODY>DIV:nth-of-type(3)>DIV>DIV>DIV>DIV:nth-of-type(3)>BUTTON" BUTTON=0
+TAG POS=1 TYPE=BUTTON ATTR=TXT:Подписаться
+TAB CLOSE
+
+	`);
+
+
+
+
+
+
